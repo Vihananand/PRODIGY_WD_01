59cd9511587d0d993e04292f0d645b94d57904a3
Vihan Anand <63580200+Vihananand@users.noreply.github.com>
2024-07-11 22:43:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('.nav-link').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.style.color = '#ff9800';
+    });
+    item.addEventListener('mouseout', () => {
+        item.style.color = 'white';
+    });
+
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - document.querySelector('.navbar').offsetHeight,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
